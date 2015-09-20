82bee30c0a91a33994acc1a2d298901d68c62550
Oleksandr_Hnyp@epam.com <Oleksandr_Hnyp@epam.com>
2015-09-20 20:44:06 +0300

    java8: task1

--- Module1/hw2/cdp_jamp_2015_java8/src/main/java/com/epam/cdp/java/Tasks.java
+++ Module1/hw2/cdp_jamp_2015_java8/src/main/java/com/epam/cdp/java/Tasks.java
@@ -1,22 +1,51 @@
 package com.epam.cdp.java;
 
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Tasks {
 
     private static final Pattern ALPHABETIC_RPL = Pattern.compile("[^a-zA-Z]");
-    // ALPHABETIC_RPL.matcher(w).replaceAll("")
-
+    
+    private static final int WORD_LENGTH_MIN = 5;
+    
+    private static final int WORD_LENGTH_MAX = 25;
+    
     public static List<String> task1Java7(List<String> words) {
-        //TODO your code here
-        return null;
+        List<String> filteredWords = new ArrayList<>();
+        for (String word : words) {
+        	String wordAlpha = leaveAlphabeticalSymbolsOnly(word);
+        	if (checkWordLength(wordAlpha)) {
+        		filteredWords.add(wordAlpha.toLowerCase());
+        	}
+        }
+        return filteredWords;
+    }
+    
+    private static boolean checkWordLength(String word) {
+    	return word.length() >= WORD_LENGTH_MIN 
+    			&& word.length() <= WORD_LENGTH_MAX;
+    }
+    
+    private static String leaveAlphabeticalSymbolsOnly(String source) {
+    	return ALPHABETIC_RPL.matcher(source).replaceAll("");
     }
 
+
     public static List<String> task1Java8(List<String> words, boolean isParallel) {
-        //TODO your code here
-        return null;
+    	Stream<String> wordsStream = words.stream();
+    	if (isParallel) {
+    		wordsStream = wordsStream.parallel();
+    	} 
+
+        return wordsStream.map(Tasks::leaveAlphabeticalSymbolsOnly)
+            	.filter(Tasks::checkWordLength)
+            	.map((word) -> word.toLowerCase())
+            	.collect(Collectors.toList());
     }
 
     public static List<String> task2Java7(List<String> words) {
@@ -33,5 +62,5 @@ public class Tasks {
         //TODO your code here
         return null;
     }
-
+    
 }
