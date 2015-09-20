07c353c8cf60d90137327fc41d0404d5152ea80f
Oleksandr_Hnyp@epam.com <Oleksandr_Hnyp@epam.com>
2015-09-20 21:12:23 +0300

    java8: task2 (task1 little refactor)

--- Module1/hw2/cdp_jamp_2015_java8/src/main/java/com/epam/cdp/java/Tasks.java
+++ Module1/hw2/cdp_jamp_2015_java8/src/main/java/com/epam/cdp/java/Tasks.java
@@ -2,6 +2,8 @@ package com.epam.cdp.java;
 
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -15,47 +17,79 @@ public class Tasks {
     
     private static final int WORD_LENGTH_MAX = 25;
     
+    private static final int FILTERED_WORDS_MAX_SIZE = 70_000;
+    
     public static List<String> task1Java7(List<String> words) {
         List<String> filteredWords = new ArrayList<>();
         for (String word : words) {
-        	String wordAlpha = leaveAlphabeticalSymbolsOnly(word);
-        	if (checkWordLength(wordAlpha)) {
-        		filteredWords.add(wordAlpha.toLowerCase());
-        	}
+            String wordAlpha = leaveAlphabeticalSymbolsOnly(word);
+            if (checkWordLength(wordAlpha)) {
+                filteredWords.add(wordAlpha.toLowerCase());
+            }
         }
         return filteredWords;
     }
     
     private static boolean checkWordLength(String word) {
-    	return word.length() >= WORD_LENGTH_MIN 
-    			&& word.length() <= WORD_LENGTH_MAX;
+        return word.length() >= WORD_LENGTH_MIN 
+                && word.length() <= WORD_LENGTH_MAX;
     }
     
     private static String leaveAlphabeticalSymbolsOnly(String source) {
-    	return ALPHABETIC_RPL.matcher(source).replaceAll("");
+        return ALPHABETIC_RPL.matcher(source).replaceAll("");
     }
 
 
     public static List<String> task1Java8(List<String> words, boolean isParallel) {
-    	Stream<String> wordsStream = words.stream();
-    	if (isParallel) {
-    		wordsStream = wordsStream.parallel();
-    	} 
-
-        return wordsStream.map(Tasks::leaveAlphabeticalSymbolsOnly)
-            	.filter(Tasks::checkWordLength)
-            	.map((word) -> word.toLowerCase())
-            	.collect(Collectors.toList());
+        return getStream(words, isParallel)
+                .map(Tasks::leaveAlphabeticalSymbolsOnly)
+                .filter(Tasks::checkWordLength)
+                .map((word) -> word.toLowerCase())
+                .collect(Collectors.toList());
+    }
+    
+    private static <T> Stream<T> getStream(List<T> source, boolean isParallel) {
+        if (isParallel) {
+            return source.stream().parallel();
+        }
+        return source.stream();
     }
 
     public static List<String> task2Java7(List<String> words) {
-        //TODO your code here
-        return null;
+        List<String> filteredWords = new ArrayList<>();
+        for (String word : words) {
+            String wordAlpha = leaveAlphabeticalSymbolsOnly(word);
+            if (checkWordLength(wordAlpha)) {
+                filteredWords.add(wordAlpha.toLowerCase());
+            }
+        }
+        Collections.sort(filteredWords, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o2.length() - o1.length();
+            }
+        });
+        Collections.sort(filteredWords, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.length() - o2.length();
+            }
+        });
+        if (filteredWords.size() > FILTERED_WORDS_MAX_SIZE) {
+            filteredWords.subList(0, FILTERED_WORDS_MAX_SIZE - 1);
+        }
+        return filteredWords;
     }
 
     public static List<String> task2Java8(List<String> words, boolean isParallel) {
-        //TODO your code here
-        return null;
+        return getStream(words, isParallel)
+                .map(Tasks::leaveAlphabeticalSymbolsOnly)
+                .filter(Tasks::checkWordLength)
+                .map((word) -> word.toLowerCase())
+                .sorted((w1, w2) -> w2.length() - w1.length())
+                .sorted((w1, w2) -> w1.length() - w2.length())
+                .limit(FILTERED_WORDS_MAX_SIZE)
+                .collect(Collectors.toList());
     }
 
     public static List<String> task3(Path... paths) {
