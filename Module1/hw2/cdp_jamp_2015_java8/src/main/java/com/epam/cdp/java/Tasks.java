ada4d2c786f8784cf3a646e35a63f73b274fad6e
Oleksandr_Hnyp@epam.com <Oleksandr_Hnyp@epam.com>
2015-09-27 19:51:39 +0300

    java8: task3 - top words

--- Module1/hw2/cdp_jamp_2015_java8/src/main/java/com/epam/cdp/java/Tasks.java
+++ Module1/hw2/cdp_jamp_2015_java8/src/main/java/com/epam/cdp/java/Tasks.java
@@ -1,10 +1,16 @@
 package com.epam.cdp.java;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -18,6 +24,14 @@ public class Tasks {
     private static final int WORD_LENGTH_MAX = 25;
     
     private static final int FILTERED_WORDS_MAX_SIZE = 70_000;
+    
+    private static final String WORDS_SPLIT_PATTERN = "[^\\w]+";
+    
+    private static final int TOP_WORDS_MAX_COUNT = 10;
+    
+    private static final String TASK3_FILE_1 = "samples/task3.txt";
+    
+    private static final String TASK3_FILE_2 = "samples/task3_1.txt";
     
     public static List<String> task1Java7(List<String> words) {
         List<String> filteredWords = new ArrayList<>();
@@ -82,19 +96,55 @@ public class Tasks {
     }
 
     public static List<String> task2Java8(List<String> words, boolean isParallel) {
+        Comparator<String> stringLengthDescComparator = (w1, w2) -> w2.length() - w1.length();
         return getStream(words, isParallel)
                 .map(Tasks::leaveAlphabeticalSymbolsOnly)
                 .filter(Tasks::checkWordLength)
                 .map((word) -> word.toLowerCase())
-                .sorted((w1, w2) -> w2.length() - w1.length())
-                .sorted((w1, w2) -> w1.length() - w2.length())
+                .sorted(stringLengthDescComparator)
+                .sorted(stringLengthDescComparator.reversed())
                 .limit(FILTERED_WORDS_MAX_SIZE)
                 .collect(Collectors.toList());
     }
 
-    public static List<String> task3(Path... paths) {
-        //TODO your code here
-        return null;
+    public static List<String> task3(Path... paths)  {
+        Map<String, Integer> wordsMap = new HashMap<>();
+        //read and populate words and their count to a map
+        Arrays.stream(paths)
+            .flatMap(Tasks::getStreamOfLines)
+            .flatMap(line -> Stream.of(line.split(WORDS_SPLIT_PATTERN)))
+            .map(String::toLowerCase)
+            .forEach(word -> Tasks.addWordToMap(word, wordsMap));
+        //sort map with words by word count (desc) and return top words
+        return wordsMap.entrySet().stream()
+            .sorted((e1, e2) -> e2.getValue().compareTo(e1.getValue()))
+            .limit(TOP_WORDS_MAX_COUNT)
+            .map(entry -> entry.getKey())
+            .collect(Collectors.toList());
+    }
+    
+    private static Stream<String> getStreamOfLines(Path path) {
+        try {
+            return Files.lines(path);
+        } catch (IOException e) {
+            System.err.println("Failed to read path " + path.toString());
+            return Stream.empty();
+        }   
+    }
+    
+    private static void addWordToMap(String word, Map<String, Integer> wordsMap) {
+        if (wordsMap.containsKey(word)) {
+            wordsMap.put(word, wordsMap.get(word) + 1);
+        } else {
+            wordsMap.put(word, 1);
+        }
+    }
+    
+    public static void main(String[] args) {
+        List<String> topWords = task3(Paths.get(TASK3_FILE_1),
+                Paths.get(TASK3_FILE_2));
+        System.out.println("Task3 top words:");
+        System.out.println(Arrays.toString(topWords.toArray()));
     }
     
 }
